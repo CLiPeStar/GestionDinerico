f23a40f4c23e17799115f511f9ba0552d5fdfa72
Alonso Bermejo <59649469+CLiPeStar@users.noreply.github.com>
2021-02-24 13:08:12 +0100

    Formulario de busqueda

--- src/app/Pages/buscar/buscar.page.ts
+++ src/app/Pages/buscar/buscar.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-buscar',
@@ -7,11 +8,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BuscarPage implements OnInit {
   private _titulo: string = 'Buscar';
+  private _page: string = 'Gastos';
+  private formGroup: FormGroup;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  segmentChanged(ev: any) {
+
+  }
+
+  private generateForm(){
+    this.formGroup= new FormGroup({
+
+
+      }
+    )
+  }
+
+  set page(value: string) {
+    this._page = value;
+  }
+
+  get page() {
+    return this._page;
   }
 
   get titulo(): string {
