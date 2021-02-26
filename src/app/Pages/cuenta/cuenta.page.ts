9e80d2e819ec48e6fc1d399c2297b7a67af6222f
Alonso Bermejo <59649469+CLiPeStar@users.noreply.github.com>
2021-02-26 18:33:16 +0100

    diagrama añadido

--- src/app/Pages/cuenta/cuenta.page.ts
+++ src/app/Pages/cuenta/cuenta.page.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-cuenta',
@@ -7,11 +8,52 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CuentaPage implements OnInit {
   private _titulo: string = 'Cuenta';
-
+  @ViewChild('myChart') myDiv: ElementRef;
   constructor() {
   }
 
   ngOnInit() {
+    this.generateCharts();
+  }
+
+
+  generateCharts() {
+    const myChart = new Chart(this.myDiv, {
+      type: 'bar',
+      data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        datasets: [{
+       //   label: '',
+          data: [12, 19, 3, 5, 2, 3, 5, 2, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
   get titulo(): string {
